40db714b5a33cb6f2fdd11160abb06c238686e34
Rafael <rflernesto@gmail.com>
2023-03-16 20:44:43 -0400

    chore: versioning bucket

--- kops/shell/s3-config-bucket.sh
+++ kops/shell/s3-config-bucket.sh
@@ -3,3 +3,5 @@
 aws s3api create-bucket \
     --bucket devlab-kops-state-store \
     --region us-east-1
+
+aws s3api put-bucket-versioning --bucket devlab-kops-state-store  --versioning-configuration Status=Enabled
