724043204487e3dbd9d7bf3e671660813af7c50c
Rafael <rflernesto@gmail.com>
2023-03-16 21:07:16 -0400

    chore: add statement to encrypt the bucket

--- kops/shell/s3-config-bucket.sh
+++ kops/shell/s3-config-bucket.sh
@@ -6,3 +6,7 @@ aws s3api create-bucket \
 
 aws s3api put-bucket-versioning --bucket devlab-kops-state-store  --versioning-configuration Status=Enabled
 
+#uncomment to encrypt the bucket
+#aws s3api put-bucket-encryption --bucket devlab-kops-state-store --server-side-encryption-configuration '{"Rules":[{"ApplyServerSideEncryptionByDefault":{"SSEAlgorithm":"AES256"}}]}'
+
+
